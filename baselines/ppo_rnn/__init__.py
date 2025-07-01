5117c3dcdcb0458544fbab4c927b2e91f3763bdc
Fiona Luo <fiona.luo@gmail.com>
2025-06-30 23:41:05 -0400

    move ppo lstm, rnn to a branch for now & delete from main

--- baselines/ppo_rnn/__init__.py
+++
@@ -1,7 +0,0 @@
-# PPO RNN baseline implementation
-# TODO: Implement PPO with RNN for handling sequential observations 
-
-from .agent import PPORnnAgent
-from .ppo_rnn import PPORnnTrainer
-
-__all__ = ['PPORnnAgent', 'PPORnnTrainer'] 
